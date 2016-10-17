10ac1e6d268bf040b5b36be725828f266739ce61
Gregory <grihabor@mail.ru>
2016-10-17 22:36:47 +0300

    optimizations Rewrote gendata, check for file existance in get params

--- task_02/util/get_params.py
+++ task_02/util/get_params.py
@@ -6,11 +6,14 @@ except IndexError:
     print('Failed to get params')
     exit(0)
 
-with open(filename) as f:
-    for line in f:
-        line = line.split()
-        if len(line) < 4:
-            continue
+try:
+    with open(filename) as f:
+        for line in f:
+            line = line.split()
+            if len(line) < 4:
+                continue
 
-        if line[0] == 'int' and line[1].upper() == line[1]:
-            print(line[1], '=', line[3][:-1])
+            if line[0] == 'int' and line[1].upper() == line[1]:
+                print(line[1], '=', line[3][:-1])
+except FileNotFoundError as e:
+    print(e)
